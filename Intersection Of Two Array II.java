2db188b555381275ec8bd280d0850a6753b0f288
HARIPRASANTH T <92248026+ihariprasanth@users.noreply.github.com>
2026-02-08 10:14:10 +0530

    Implement intersection of two arrays

---
+++ Intersection Of Two Array II.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        ArrayList<Integer> res = new ArrayList<>();
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                res.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        int[] ans = new int[res.size()];
+        for (int k = 0; k < res.size(); k++) ans[k] = res.get(k);
+        return ans;
+    }
+}
